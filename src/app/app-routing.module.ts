c59b75e6711de2cdd4e3ff6cd79d5126d66e30e8
Fouad Abdelhalim <fouadabdelhalim9@gmail.com>
2024-12-04 23:43:21 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AsideComponent } from './aside/aside.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { WorkComponent } from './work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' ,redirectTo:'home' ,pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'skills', component:SkillsComponent},
+  {path:'experience', component:ExperienceComponent},
+  {path:'work', component:WorkComponent},
+  {path:'**', component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AsideComponent } from './aside/aside.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AsideComponent,
+    NotfoundComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
